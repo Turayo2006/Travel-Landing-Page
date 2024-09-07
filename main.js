c20f27785bcfcd1c1e1670e432f5e2eff5dab223
Tunrayo <120662177+Turayo2006@users.noreply.github.com>
2024-09-07 06:58:38 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+// const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen?"ri-close-line":"ri-menu-line")
+
+});
+
+navLinks.addEventListener("click", (e) =>{
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("click", "ri-menu-line");
+
+});
+
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+scrollReveal().reveal(".header__image img", {
+...scrollRevealOption,
+origin: "right",
+});
+
+scrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+
+});
